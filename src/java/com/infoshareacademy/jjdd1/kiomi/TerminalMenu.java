4995abfccf49467bcd1c5fc9baec9908cf278722
Arkadiusz Zielazny <azielazny@gmail.com>
2017-03-31 16:42:01 +0200

    hello world

---
+++ src/java/com/infoshareacademy/jjdd1/kiomi/TerminalMenu.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy.jjdd1.kiomi;
+
+/**
+ * Created by arkadiuszzielazny on 31.03.17.
+ */
+public class TerminalMenu {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
