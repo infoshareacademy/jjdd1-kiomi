88cfad46b439be3c37cc53252987a991f576977a
Karol Kolanczak <karol.kolanczak@hotmail.com>
2017-04-06 15:08:17 +0200

    Terminal choice of Customer of Model of the car added

--- src/java/com/infoshareacademy/jjdd1/kiomi/TerminalMenu.java
+++ src/java/com/infoshareacademy/jjdd1/kiomi/TerminalMenu.java
@@ -10,10 +10,15 @@ import java.util.Scanner;
 public class TerminalMenu {
 
     static String clientsCarBrand;
+    static String clientsCarModel;
     static String originalClientsCarBrand;
+    static String originalClientsCarModel;
     static private List<String> listOfCarBrands;
+    static private List<String> listOfCarModels;
     static boolean isCarBrandOnOurList;
+    static boolean isCarModelOnOurList;
     static String brandOfCarFromDatabase;
+    static Scanner scanner;
 
     public static void main(String[] args) {
 
@@ -22,8 +27,15 @@ public class TerminalMenu {
         //Strona powitalna aplikacji konsolowej Autoparts KIOMI
         System.out.print("Welcome to 'Autoparts KIOMI'. ");
 
+        scanner = new Scanner(System.in);
+
         clientBrandCar();
 
+        setListOfCarModels();
+
+        clientModelCar();
+
+        scanner.close();
     }
 
     static String clientBrandCar() {
@@ -34,7 +46,6 @@ public class TerminalMenu {
             System.out.println(" - " + value);
         }
 
-        Scanner scanner = new Scanner(System.in);
         originalClientsCarBrand = scanner.next();
 
         clientsCarBrand = originalClientsCarBrand.toLowerCase();
@@ -43,14 +54,12 @@ public class TerminalMenu {
 
         validationOfClientsCarBrand();
 
-        scanner.close();
-
         return clientsCarBrand;
     }
 
     static void validationOfClientsCarBrand() {
 
-        isChoosenCarModelOnOurList();
+        isChoosenCarBrandOnOurList();
 
         if (isCarBrandOnOurList == false) {
             System.out.println(" | Attention: '" + originalClientsCarBrand + "' is not on our list");
@@ -59,11 +68,26 @@ public class TerminalMenu {
         else if (isCarBrandOnOurList == true) {
             System.out.println(" | Success: '" + originalClientsCarBrand + "' is on our list");
             System.out.println("--------------------------------------------------");
-            System.out.println("Your current choise is: Brand: '" + clientsCarBrand + "' | Model: '" + "' | ");
+            System.out.println("Your current choise is: Brand: '" + clientsCarBrand+"'" );
         }
     }
 
-    private static boolean isChoosenCarModelOnOurList() {
+    static void validationOfClientsCarModel() {
+
+        isChoosenCarModelOnOurList();
+
+        if (isCarModelOnOurList == false) {
+            System.out.println(" | Attention: '" + originalClientsCarModel + "' is not on our list");
+            clientModelCar();
+        }
+        else if (isCarModelOnOurList == true) {
+            System.out.println(" | Success: '" + originalClientsCarModel + "' is on our list");
+            System.out.println("--------------------------------------------------");
+            System.out.println("Your current choise is: Brand: '" + clientsCarBrand+ "' | Model: '" + clientsCarModel+ "' | ");
+        }
+    }
+
+    private static boolean isChoosenCarBrandOnOurList() {
         for (String value : listOfCarBrands) {
 
             if (value.equals(clientsCarBrand)) {
@@ -77,9 +101,67 @@ public class TerminalMenu {
         return isCarBrandOnOurList;
     }
 
+    private static boolean isChoosenCarModelOnOurList() {
+        for (String value : listOfCarModels) {
+
+            if (value.equals(clientsCarModel)) {
+                isCarModelOnOurList = true;
+                break;
+            } else {
+                isCarModelOnOurList = false;
+            }
+        }
+        return isCarModelOnOurList;
+    }
+
+    static String clientModelCar() {
+
+        System.out.println("Choose a model of the car from the list:");
+
+        for (String value : listOfCarModels) {
+            System.out.println(" - " + value);
+        }
+
+        originalClientsCarModel = scanner.next();
+
+        clientsCarModel = originalClientsCarModel.toLowerCase();
+
+        System.out.print("You have choosen model of the car: '" + originalClientsCarModel + "'");
+
+        validationOfClientsCarModel();
+
+        return clientsCarModel;
+    }
+
+
     static void setListOfCarBrands() {
         listOfCarBrands = new ArrayList<String>();
         listOfCarBrands.add("ford");
         listOfCarBrands.add("kia");
     }
+
+    public static void setListOfCarModels() {
+        listOfCarModels = new ArrayList<String>();
+
+        if( clientsCarBrand.equals("ford")){
+
+            listOfCarModels.add("BANTAM");
+            listOfCarModels.add("CONSUL");
+            listOfCarModels.add("CORTINA");
+            listOfCarModels.add("focus");
+            listOfCarModels.add("GRANADA");
+            listOfCarModels.add("SCORPIO");
+        }
+
+        else if(clientsCarBrand.equals("kia")){
+            listOfCarModels.add("AMANTI (GH)");
+            listOfCarModels.add("AVELLA");
+            listOfCarModels.add("BESTA Autobus");
+            listOfCarModels.add("JOICE");
+            listOfCarModels.add("MAGENTIS");
+            listOfCarModels.add("PICANTO");
+            listOfCarModels.add("rio");
+        }
+    }
+
 }
