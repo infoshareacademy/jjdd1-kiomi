1cbbb6a37288b8ba2816a0cb742cf4e4147ca22b
Karol Kolanczak <karol.kolanczak@hotmail.com>
2017-04-06 11:59:35 +0200

    updated Command in Terminal concerning choice of Custumer of Brand of the car

--- src/java/com/infoshareacademy/jjdd1/kiomi/TerminalMenu.java
+++ src/java/com/infoshareacademy/jjdd1/kiomi/TerminalMenu.java
@@ -1,12 +1,85 @@
 package com.infoshareacademy.jjdd1.kiomi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by arkadiuszzielazny on 31.03.17.
  */
 public class TerminalMenu {
+
+    static String clientsCarBrand;
+    static String originalClientsCarBrand;
+    static private List<String> listOfCarBrands;
+    static boolean isCarBrandOnOurList;
+    static String brandOfCarFromDatabase;
+
     public static void main(String[] args) {
 
+        setListOfCarBrands();
 
-        System.out.println("Hello");
+        //Strona powitalna aplikacji konsolowej Autoparts KIOMI
+        System.out.print("Welcome to 'Autoparts KIOMI'. ");
+
+        clientBrandCar();
+
+    }
+
+    static String clientBrandCar() {
+
+        System.out.println("Choose a brand of the car from the list:");
+
+        for (String value : listOfCarBrands) {
+            System.out.println(" - " + value);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        originalClientsCarBrand = scanner.next();
+
+        clientsCarBrand = originalClientsCarBrand.toLowerCase();
+
+        System.out.print("You have choosen brand of the car: '" + originalClientsCarBrand + "'");
+
+        validationOfClientsCarBrand();
+
+        scanner.close();
+
+        return clientsCarBrand;
+    }
+
+    static void validationOfClientsCarBrand() {
+
+        isChoosenCarModelOnOurList();
+
+        if (isCarBrandOnOurList == false) {
+            System.out.println(" | Attention: '" + originalClientsCarBrand + "' is not on our list");
+            clientBrandCar();
+        }
+        else if (isCarBrandOnOurList == true) {
+            System.out.println(" | Success: '" + originalClientsCarBrand + "' is on our list");
+            System.out.println("--------------------------------------------------");
+            System.out.println("Your current choise is: Brand: '" + clientsCarBrand + "' | Model: '" + "' | ");
+        }
+    }
+
+    private static boolean isChoosenCarModelOnOurList() {
+        for (String value : listOfCarBrands) {
+
+            if (value.equals(clientsCarBrand)) {
+                isCarBrandOnOurList = true;
+                break;
+            } else {
+                isCarBrandOnOurList = false;
+            }
+        }
+
+        return isCarBrandOnOurList;
+    }
+
+    static void setListOfCarBrands() {
+        listOfCarBrands = new ArrayList<String>();
+        listOfCarBrands.add("ford");
+        listOfCarBrands.add("kia");
     }
 }
