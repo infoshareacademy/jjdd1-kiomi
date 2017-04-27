9238c3ae0372573387c14d27d484c2f643f89059
azielazny <azielazny@gmail.com>
2017-04-27 09:51:07 +0200

    add admin emails class

---
+++ src/main/java/com/infoshareacademy/jjdd1/kiomi/app/model/cars/AdministratorEmails.java
@@ -0,0 +1,20 @@
+package com.infoshareacademy.jjdd1.kiomi.app.model.cars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by arek50 on 2017-04-27.
+ */
+public class AdministratorEmails {
+
+    private static List<String> emailsList = new ArrayList<String>() {
+        {
+            add("azielazny@gmail.com");
+        }
+    };
+
+    public static int getIsAdministrator(String email) {
+        return emailsList.contains(email) ? 1 : 0;
+    }
+}
---
+++ src/main/java/com/infoshareacademy/jjdd1/kiomi/app/services/CarFromAztecCode.java
@@ -0,0 +1,7 @@
+package com.infoshareacademy.jjdd1.kiomi.app.services;
+
+/**
+ * Created by arek50 on 2017-04-25.
+ */
+public class CarFromAztecCode {
+}
