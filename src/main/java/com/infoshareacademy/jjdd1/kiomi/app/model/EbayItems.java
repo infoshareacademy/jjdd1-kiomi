f4e4d8845d5e3f4497c6ab9b07016e86399a9291
azielazny <azielazny@gmail.com>
2017-05-17 23:32:00 +0200

    first try with ebay API

---
+++ src/main/java/com/infoshareacademy/jjdd1/kiomi/app/model/EbayItems.java
@@ -0,0 +1,77 @@
+package com.infoshareacademy.jjdd1.kiomi.app.model;
+
+import java.util.Arrays;
+
+/**
+ * Created by arek50 on 2017-05-17.
+ */
+public class EbayItems {
+    private String[] itemId;
+    private String[] title;
+    private String[] viewItemUrl;
+    private String[] paymentMethod;
+    private String[] location;
+    private String[] country;
+//    private String sellingStatus;
+
+
+    public String[] getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String[] itemId) {
+        this.itemId = itemId;
+    }
+
+    public String[] getTitle() {
+        return title;
+    }
+
+    public void setTitle(String[] title) {
+        this.title = title;
+    }
+
+    public String[] getViewItemUrl() {
+        return viewItemUrl;
+    }
+
+    public void setViewItemUrl(String[] viewItemUrl) {
+        this.viewItemUrl = viewItemUrl;
+    }
+
+    public String[] getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String[] paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String[] getLocation() {
+        return location;
+    }
+
+    public void setLocation(String[] location) {
+        this.location = location;
+    }
+
+    public String[] getCountry() {
+        return country;
+    }
+
+    public void setCountry(String[] country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "EbayItems{" +
+                "itemId=" + Arrays.toString(itemId) +
+                ", title=" + Arrays.toString(title) +
+                ", viewItemUrl=" + Arrays.toString(viewItemUrl) +
+                ", paymentMethod=" + Arrays.toString(paymentMethod) +
+                ", location=" + Arrays.toString(location) +
+                ", country=" + Arrays.toString(country) +
+                '}';
+    }
+}
