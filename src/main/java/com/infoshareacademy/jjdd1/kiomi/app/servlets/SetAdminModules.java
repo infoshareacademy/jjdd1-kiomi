4f96e4f83647b30ac49cd96a0574926c77c37927
azielazny <azielazny@gmail.com>
2017-05-24 09:52:27 +0200

    reserve class name for adminpanel

---
+++ src/main/java/com/infoshareacademy/jjdd1/kiomi/app/servlets/SetAdminModules.java
@@ -0,0 +1,50 @@
+package com.infoshareacademy.jjdd1.kiomi.app.servlets;
+
+import com.infoshareacademy.jjdd1.kiomi.app.model.cars.BrandsCache;
+import com.infoshareacademy.jjdd1.kiomi.app.services.CarsDataLoader2;
+import com.infoshareacademy.jjdd1.kiomi.app.services.SessionData;
+
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by arek50 on 2017-05-05.
+ */
+@WebServlet(urlPatterns = "/setadminmodules")
+public class SetAdminModules extends HttpServlet {
+    @Inject
+    CarsDataLoader2 carsDataLoader2;
+    @Inject
+    BrandsCache brandsCache;
+    @Inject
+    SessionData sessionData;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (sessionData.isLogged() == false) {
+            req.setAttribute("error", "Nie ma takiego użytkownika. Dostęp zabroniony.");
+            resp.sendRedirect("http://localhost:8080/googlelogin");
+        }
+
+        req.setCharacterEncoding("UTF-8");
+        String brandName = sessionData.getCar().getBrand().getName();
+        String modelName = sessionData.getCar().getModel().getName();
+        String carTypeName = sessionData.getCar().getCarType().getName();
+
+        req.setAttribute("brand", brandName);
+        req.setAttribute("model", modelName);
+        req.setAttribute("carType", carTypeName);
+        req.setAttribute("sessionUserName", sessionData.getFirstname());
+//            req.setAttribute("errorMessage", errorMessage);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("SetAdminModules.jsp");
+        dispatcher.forward(req, resp);
+
+    }
+}
